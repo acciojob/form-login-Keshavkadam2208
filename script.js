e428240aeb8524d0c2266b853ab9b086103e9d66
Keshavkadam2208 <131426561+Keshavkadam2208@users.noreply.github.com>
2025-06-23 18:26:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function getFormvalue() {
     //Write your code here
-	const form = document.getElementById("form1");
-	const firstname = form.elements["fname"].value.trim();
-	const lastname = form.elements["lname"].value.trim();
-	const fullname = '${firstname} ${lastname}'.trim();
-	alert(fullname);
-	return false;
+   const form = document.getElementById("form1");
+      const firstName = form.elements["fname"].value.trim();
+      const lastName = form.elements["lname"].value.trim();
+      const fullName = `${firstName} ${lastName}`.trim(); // avoids extra space if one field is empty
+      alert(fullName);
+      return false;
 
 }
