5473ccc52b347ce8315bb0192e2341065b34010d
Keshavkadam2208 <131426561+Keshavkadam2208@users.noreply.github.com>
2025-06-23 18:20:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function getFormvalue() {
     //Write your code here
-	const form = document.getElementById("form");
+	const form = document.getElementById("form1");
 	const firstname = form.elements["fname"].value.trim();
 	const lastname = form.elements["lname"].value.trim();
 	const fullname = '${firstname} ${lastname}'.trim();
